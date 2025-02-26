4edf9117f215d557a3925ec8e720622c3b049d65
Lucas Paim <lucaspaim924@gmail.com>
2025-02-26 09:14:05 -0300

    Atualização estudo python

---
+++  modulos_e_pacotes_22/desafios/desafio107.py
@@ -0,0 +1,10 @@
+# Desafio 107: Crie um módulo chamado moeda.py que tenha funções incorporadas aumentar(), diminuir(), dobro() e metade().
+# Faça também um programa que importe esse módulo e use algumas dessas funções
+
+from ex107 import moeda
+
+p = float(input('Digite o preço: R$'))
+print(f'A metade de R${p:.2f} é R${moeda.metade(p):.2f}')
+print(f'O dobro de R${p:.2f} é R${moeda.dobro(p):.2f}')
+print(f'Aumentando 10%, temos R${moeda.aumentar(p, 10):.2f}')
+print(f'Reduzindo 13%, temos R${moeda.diminuir(p, 13):.2f}')
---
+++  modulos_e_pacotes_22/desafios/ex107/moeda.py
@@ -0,0 +1,11 @@
+def metade(p):
+    return p * 0.5
+
+def dobro(p):
+    return p * 2
+
+def aumentar(p, n=0):
+    return p * (1 + n/100)
+
+def diminuir(p, n=0):
+    return p * (1 - n/100)
